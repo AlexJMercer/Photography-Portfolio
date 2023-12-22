4b659dee64f79b1a8ef411ef7677a1407ceb45c2
Tathagata Guha Ray <nguharay71@gmail.com>
2023-12-22 22:10:11 +0530

    gallery main in progress, add img

---
+++ js/requests.js
@@ -0,0 +1,31 @@
+
+// Function to get a card element and then send 
+// the title name to another page to display images
+
+function sendRequest(element) {
+    const cardTitle = element.parentElement.querySelector('.card-title').innerText;
+
+    const encodedTitle = cardTitle;
+
+    const url = 'gallery-display.html';
+    const data = { request: encodedTitle };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Request sent successfully');
+            window.location.href = url; // Redirect to the specified page
+        } else {
+            console.log('Failed to send request');
+        }
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+}
