01a9d24d51a5e8e0ebadbf7a619f2d4ad987903c
Tathagata Guha Ray <nguharay71@gmail.com>
2023-12-22 22:54:05 +0530

    test #2

--- gallery-display.php
+++ gallery-display.php
@@ -1,37 +1,30 @@
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['req'])) {
-        $folderName = $_POST['req'];
-        $folderPath = './gallery-content/' . $folderName; // Set your folder path here
-
-        if (is_dir($folderPath)) {
-            $files = scandir($folderPath);
-            foreach ($files as $file) {
-                if ($file !== '.' && $file !== '..') {
-                    if (pathinfo($file, PATHINFO_EXTENSION) === 'jpg' || pathinfo($file, PATHINFO_EXTENSION) === 'png') {
-                        echo '<img src="' . $folderPath . '/' . $file . '" alt="' . $file . '" />';
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    
-    <title><?php echo $folderName; ?></title>
+    <title><?php echo $folderName . ' Gallery'; ?></title>
 </head>
 <body>
-    <h1>hello w0rlD</h1>
+    <h1>Gallery</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['req'])) {
+            $folderName = $_POST['req'];
+            $folderPath = './gallery-content/' . $folderName; // Set your folder path here
+
+            if (is_dir($folderPath)) {
+                $files = scandir($folderPath);
+                foreach ($files as $file) {
+                    if ($file !== '.' && $file !== '..') {
+                        if (pathinfo($file, PATHINFO_EXTENSION) === 'jpg' || pathinfo($file, PATHINFO_EXTENSION) === 'png') {
+                            echo '<img src="' . $folderPath . '/' . $file . '" alt="' . $file . '" />';
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ?>
 </body>
 </html>
