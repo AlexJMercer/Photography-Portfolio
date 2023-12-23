2c14c7a73691403528b05e8e00a4f6df04b8d370
Tathagata Guha Ray <nguharay71@gmail.com>
2023-12-23 23:03:12 +0530

    working POST request

--- gallery-display.php
+++ gallery-display.php
@@ -3,28 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title><?php echo $folderName . ' Gallery'; ?></title>
 </head>
 <body>
     <h1>Gallery</h1>
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        if (isset($_GET['req'])) {
-            $folderName = $_GET['req'];
-            $folderPath = './gallery-content/' . $folderName; // Set your folder path here
 
-            if (is_dir($folderPath)) {
-                $files = scandir($folderPath);
-                foreach ($files as $file) {
-                    if ($file !== '.' && $file !== '..') {
-                        if (pathinfo($file, PATHINFO_EXTENSION) === 'jpg' || pathinfo($file, PATHINFO_EXTENSION) === 'png') {
-                            echo '<img src="' . $folderPath . '/' . $file . '" alt="' . $file . '" />';
-                        }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['folderName'])) {
+        
+        $folderName = $_POST['folderName'];
+        $folderPath = './gallery-content/' . $folderName;    
+
+        if (is_dir($folderPath)) {
+            $files = scandir($folderPath);
+            foreach ($files as $file) {
+                if ($file !== '.' && $file !== '..') {
+                    if (pathinfo($file, PATHINFO_EXTENSION) === 'jpg' || pathinfo($file, PATHINFO_EXTENSION) === 'png') {
+                        echo '<img src="' . $folderPath . '/' . $file . '" alt="' . $file . '" >';
                     }
                 }
             }
+        } else {
+            echo '<h3>
+                <center>Folder not found</center>
+            </h3>';
         }
+    } else {
+        echo '<h3>
+            <center>Request Not Found</center>
+        </h3>';
     }
-    ?>
+} else {
+    echo '<h3>
+        <center>Invalid request</center>
+    </h3>';
+}
+
+?>
 </body>
 </html>
--- js/requests.js
+++ js/requests.js
@@ -5,23 +5,7 @@
 function sendRequest(element) {
     const cardTitle = element.parentElement.querySelector('.card-title').innerText;
 
-    // Send a GET request
-    window.location.href = 'gallery-display.php?req=' + cardTitle;
-
-    // Send a POST request
-    // const formData = new FormData();
-    // formData.append('req', cardTitle);
-
-    // const xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState === 4 && this.status === 200) {
-    //         console.log('POST request sent successfully');
-    //         window.location.href = 'gallery-display.php';
-    //     }
-    // };
-
-    // // Send a POST request
-    // xhttp.open('POST', 'gallery-display.php', true);
-    // xhttp.send(formData);
-
+    document.getElementById('postForm').querySelector('[name="folderName"]').value = cardTitle;
+    
+    document.getElementById('postForm').submit();
 }
